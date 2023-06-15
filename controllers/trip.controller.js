04f297e953e5c1dd1a97a5a9a423d9229203dce4
jgoh88 <jarrodgoh88@hotmail.com>
2023-06-15 16:36:37 +0800

    add search feature

--- controllers/trip.controller.js
+++ controllers/trip.controller.js
@@ -6,7 +6,15 @@ const {upload, uploadImage, setCloudinaryFolder} = require('../configs/cloudinar
 
 router.get('/my', async (req, res) => {
     try {
-        const trips = await Trip.find({createdBy: req.user.id}).populate('country')
+        const trips = await Trip.find({createdBy: req.user.id})
+            .populate({
+                path: 'country',
+                populate: {
+                    path: 'countryId'
+                }
+            })
+
+        console.log(trips)
         const countries = await Country.find()
         return res.render('trip/userList', {trips: trips, countries: countries})
     } catch (err) {
@@ -20,7 +28,10 @@ router.get('/favorites', async (req, res) => {
         const user = await User.findById(req.user.id).populate({
             path: 'favorites',
             populate: {
-                path: 'country'
+                path: 'country',
+                populate: {
+                    path: 'countryId'
+                }
             }
         })
         const trips = user.favorites
@@ -33,15 +44,25 @@ router.get('/favorites', async (req, res) => {
 
 router.get('/search', async (req, res) => {
     try {
-        // const trips = await Trip.find({
-        //     shared: true,
-        //     $search: {
-        //         "regex": {
-        //             "query": `${req.body.searchTerm}.*`
-        //             "path": []
-        //         }
-        //     }
-        // })
+        const keywords = req.query.searchTerm.split(' ')
+        const regex = keywords.join('|')
+        const trips = await Trip.find({
+            shared: true,
+            $or: [
+                {name: {
+                        $regex: regex, $options: 'i'
+                }},
+                {description: {
+                        $regex: regex, $options: 'i'
+                }},
+                {country: {
+                        countryName: {
+                            $regex: regex, $options: 'i'
+                        }
+                }},
+            ],
+        })
+        return res.render('trip/searchList', {trips: trips, user: req.user, searchTerm: req.query.searchTerm})
     } catch (err) {
         console.log(err)
         return res.render('error/500')
@@ -50,7 +71,7 @@ router.get('/search', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        const trip = await Trip.findById(req.params.id).populate('country')
+        const trip = await Trip.findById(req.params.id)
         return res.render('trip/show', {trip: trip})
     } catch (err) {
         console.log(err)
@@ -60,7 +81,7 @@ router.get('/:id', async (req, res) => {
 
 router.get('/:id/edit', async (req, res) => {
     try {
-        const trip = await Trip.findById(req.params.id).populate('country')
+        const trip = await Trip.findById(req.params.id)
         const countries = await Country.find()
 
         return res.render('trip/edit', {trip: trip, countries: countries})
@@ -73,6 +94,8 @@ router.get('/:id/edit', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const newTrip = new Trip({...req.body, createdBy: req.user.id})
+        const countryData = await Country.findById(newTrip.country.countryId)
+        newTrip.country.countryName = countryData.name
         await newTrip.save()
 
         // Removing below for now 
@@ -102,6 +125,8 @@ router.put('/:id', async (req, res) => {
                 place.time = new Date(d.toString().split(":")[0].slice(0,-2) + place.time)
             })
         })
+        const countryData = await Country.findById(updatedTrip.country.countryId)
+        updatedTrip.country.countryName = countryData.name
         await Trip.findByIdAndUpdate(req.params.id, {
             $set: {
                 ...updatedTrip
